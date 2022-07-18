9736724f3f4a4f9b6d85cc7f5b30154e42baefb1
JamesCahyadi <jcahyadi239@gmail.com>
2022-07-17 23:17:16 -0400

    add friend group logic

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"strings"
 
 	"github.com/JamesCahyadi/Sightstone/pkg/client"
 )
@@ -19,29 +19,80 @@ type GameInfo struct {
 
 type Friend struct {
 	Id           string   `json:"id"`
+	GroupName    string   `json:"groupName"`
 	Availability string   `json:"availability"`
 	Name         string   `json:"name"`
 	GameInfo     GameInfo `json:"lol"`
 }
 
+type FriendGroup struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 func main() {
 	lc := client.LeagueClientConnect()
+	fg := "sightstone"
 
+	// should check if the group exists or something
+	ok := findFriendGroup(lc, fg)
+	if !ok {
+		panic("Couldn't find group" + fg) // should keep listening until the group is created instead of just panicing
+	}
+
+	friends := getFriendsFromGroup(lc, fg)
+	fmt.Printf("%+v\n", friends)
+}
+
+func findFriendGroup(lc *client.LeagueClient, target string) bool {
+	var friendGroups []FriendGroup
+	resp, err := lc.Do(http.MethodGet, "/lol-chat/v1/friend-groups", nil)
+	if err != nil {
+		panic(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	err = json.Unmarshal(body, &friendGroups)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, group := range friendGroups {
+		if target == strings.ToLower(group.Name) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func getFriendsFromGroup(lc *client.LeagueClient, target string) []Friend {
 	var friends []Friend
+	// loop through all friends since lol-chat/v1/friend-groups/{id}/friend endpoint is not implemented
 	resp, err := lc.Do(http.MethodGet, "/lol-chat/v1/friends", nil)
 	if err != nil {
 		panic(err)
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer resp.Body.Close()
 
-	fmt.Println(string(body))
 	err = json.Unmarshal(body, &friends)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
+	var groupFriends []Friend
+	for _, friend := range friends {
+		if strings.ToLower(friend.GroupName) == target {
+			groupFriends = append(groupFriends, friend)
+		}
+	}
+	return groupFriends
 }
